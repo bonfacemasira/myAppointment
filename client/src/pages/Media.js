573aaa56b0b5e574150d12db06df418749426b25
Bonface Masira <bonfacemasira@gmail.com>
2022-12-15 10:38:44 +0300

    create all pages for the project

---
+++ client/src/pages/Media.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Media() {
+    return (
+      <div className="Media">Media
+      </div>
+    );
+  }
+  
+  export default Media;
+  
---
+++ client/src/pages/Packages.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Packages() {
+    return (
+      <div className="Packages">Packages
+      </div>
+    );
+  }
+  
+  export default Packages;
+  
