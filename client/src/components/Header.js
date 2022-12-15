df18626ab167ec34e661ae9fcb27f545d178d3e5
Bonface Masira <bonfacemasira@gmail.com>
2022-12-15 14:51:25 +0300

    add font-awesome icons to the header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from "react";
+import { FaFacebookF, FaTiktok, FaTwitter, FaYoutube } from "react-icons/fa";
 
 function Header() {
   return (
-    <div className="Header">Header
+    <div className="Header">
+      <a href="https://www.twitter.com" className='twitter social'>
+        <FaTwitter />
+      </a>
+      <a href="https://www.facebook.com" className='facebook social'>
+        <FaFacebookF />
+      </a>
+      <a href="https://www.tiktok.com" className='tiktok social'>
+        <FaTiktok />
+      </a>
+      <a href="https://www.youtube.com" className='youtube social'>
+        <FaYoutube />
+      </a>
     </div>
   );
 }
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 function NavBar() {
     return (
-      <div className="NavBar">NavBar
+      <div className="NavBar">
+        <div>Logo</div>
+        <div className='navigation'>
+          <h1>Home</h1>
+          <h1>About</h1>
+          <h1>Packages</h1>
+          <h1>Media</h1>
+          <h1>Contact Us</h1>
+        </div>
       </div>
     );
   }
