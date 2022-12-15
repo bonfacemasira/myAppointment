0f4f73232d124a77336ba9315bb9f5b0047c26ef
Bonface Masira <bonfacemasira@gmail.com>
2022-12-15 15:16:28 +0300

    add all required icons and style

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,54 @@
 import React from "react";
-import { FaFacebookF, FaTiktok, FaTwitter, FaYoutube } from "react-icons/fa";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTiktok,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa";
 
 function Header() {
   return (
     <div className="Header">
-      <a href="https://www.twitter.com" className='twitter social'>
-        <FaTwitter />
+      <a
+        className="instagram social"
+        target="_blank"
+        aria-label="Instagram"
+        href="https://www.instagram.com"
+      >
+        <FaInstagram size={"1.5em"} />
       </a>
-      <a href="https://www.facebook.com" className='facebook social'>
-        <FaFacebookF />
+      <a
+        className="twitter social"
+        target="_blank"
+        aria-label="Twitter"
+        href="https://www.twitter.com"
+      >
+        <FaTwitter size={"1.5em"} />
       </a>
-      <a href="https://www.tiktok.com" className='tiktok social'>
-        <FaTiktok />
+      <a
+        className="facebook social"
+        target="_blank"
+        aria-label="Facebook"
+        href="https://www.facebook.com"
+      >
+        <FaFacebookF size={"1.5em"} />
       </a>
-      <a href="https://www.youtube.com" className='youtube social'>
-        <FaYoutube />
+      <a
+        className="tiktok social"
+        target="_blank"
+        aria-label="TikTok"
+        href="https://www.tiktok.com"
+      >
+        <FaTiktok size={"1.5em"} />
+      </a>
+      <a
+        className="youtube social"
+        target="_blank"
+        aria-label="YouTube"
+        href="https://www.youtube.com"
+      >
+        <FaYoutube size={"1.5em"} />
       </a>
     </div>
   );
