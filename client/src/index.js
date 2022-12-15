44d7600de2cc32ed63b1de4b09f2c9e3deab61d4
Bonface Masira <bonfacemasira@gmail.com>
2022-12-15 12:48:41 +0300

    define routes

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home';
+import ContactUs from './pages/ContactUs';
+import Media from './pages/Media';
+import Packages from './pages/Packages';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+    <Routes>
+      <Route path="/" element={ <Home /> } />
+      <Route path="about" element={ <About/> } />
+      <Route path="contactus" element={ <ContactUs /> } />
+      <Route path="media" element={ <Media /> } />
+      <Route path="packages" element={ <Packages /> } />
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
